8caa0288740985b4614790f888bb626355ec5e0d
Davidjeje <david.marivat-larose@orange.fr>
2018-05-09 02:54:39 +0200

    Le contenu du fichier à été ajouté. Notamment mon nom et prenom, mon avatar, une phrase d'accroche, un menu pour naviguer, un formulaire de contact et un lien vers mon cv

---
+++ index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Article Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+    
+    <link href="https://fonts.googleapis.com/css?family=Grand+Hotel" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,200,300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css">
+
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    
+    <!-- Flaticons  -->
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    </head>
+    <body>
+        
+    <div class="colorlib-loader"></div>
+
+    <div id="page">
+        <nav class="colorlib-nav" role="navigation">
+            <div class="top-menu">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <div id="colorlib-logo">David Marivat</div>
+                        </div>
+                        <div class="col-xs-10 text-right menu-1">
+                            <ul>
+                                <li><a href="index.php">Accueil</a></li>
+                                <li class="has-dropdown">
+                                    <a href="blog.php">Blog</a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">Commercial</a></li>
+                                        <li><a href="#">Apartment</a></li>
+                                        <li><a href="#">House</a></li>
+                                        <li><a href="#">Building</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="blogpost.php">blog post</a></li>
+                                <li><a href="ajouterblog.php">Ajouter un blog</a></li>
+                                <li><a href="modifierblog.php">Modifier un blog</a></li>
+                                <li class="active"><a href="supprimerblog.php">Supprimer un blog</a></li>
+                                <li><a href="connexion.php">Connexion et enregistrement</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <aside id="colorlib-breadcrumbs">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 breadcrumbs text-center">
+                        <h2>Si vous voulez aller vite, allez y seul mais si vous voulez aller plus loin entourez vous des bonnes personnes qui vous aiderons à réaliser votre projet... faite appel au bon développeur.</h2>
+                    </div>
+                </div>
+            </div>
+        </aside>
+
+        <div id="colorlib-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img class="img-responsive" src="images/avatar.png" alt="avatar david">
+                    </div>
+                    <div class="col-md-6">
+                        <p>Salut ! je m'appelle <strong>David</strong>, je suis actuellement des cours d'informatique chez openclassrooms. Plus particulièrement du développement web. J'étudie les langages HTML5/CSS2, javascript, PHP. De plus dès le début de la formation j'ai pu étudier Bootstrap,Git Github et JQUERY. Par la suite j'étudirai le framework Symfony.</p>
+                        <blockquote>
+                            Cette formation est diplômante. Elle me prépare à un niveau bac 3/4 qui est enregistré au Répertoire National des Certifications Professionnelles. Openclassrooms est un établissement privé d'enseignement  à distance déclaré au rectorat de l'Académie de Paris. Elle délivre ses propres diplômes ainsi que ceux d'autres partenaires académiques prestigieux. 
+                        </blockquote>
+                        <p class="first-letra">La formation se déroule bien. Une fois par semaine je fais le point avec le mentor qui m'aide à valider mes projets.</p>
+                        <p>
+                            <ul class="colorlib-social-icons">
+                                <li><a href="#"><i class="icon-twitter"></i></a></li>
+                                <li><a href="#"><i class="icon-facebook"></i></a></li>
+                                <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                                <li><a href="davidCV.PDF"><i class="icon-dribbble"></i></a></li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+            <div id="colorlib-contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 animate-box">
+                        <h2>Contact Information</h2>
+                        <div class="row contact-info-wrap">
+                            <div class="col-md-3">
+                                <p><span><i class="icon-location-2"></i></span> 198 West 21th Street, <br> Suite 721 New York NY 10016</p>
+                            </div>
+                            <div class="col-md-3">
+                                <p><span><i class="icon-phone3"></i></span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+                            </div>
+                            <div class="col-md-3">
+                                <p><span><i class="icon-paperplane"></i></span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+                            </div>
+                            <div class="col-md-3">
+                                <p><span><i class="icon-globe"></i></span> <a href="#">yourwebsite.com</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-10 col-md-offset-1 animate-box">
+                        <h2>Get In Touch</h2>
+                        <form method="post" action="traitement.php">
+                            <div class="row form-group">
+                                <div class="col-md-6">
+                                    <label for="fname">First Name</label>
+                                    <input type="text" name="prenom" id="fname" class="form-control" placeholder="Your firstname" autofocus required>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="lname">Last Name</label>
+                                    <input type="text" name="nom" id="lname" class="form-control" placeholder="Your lastname" required>
+                                </div>
+                            </div>
+
+                            <div class="row form-group">
+                                <div class="col-md-12">
+                                    <label for="email">Email</label>
+                                    <input type="text" name="mail" id="email" class="form-control" placeholder="Your email address" required>
+                                </div>
+                            </div>
+
+                            <div class="row form-group">
+                                <div class="col-md-12">
+                                    <label for="subject">Subject</label>
+                                    <input type="text" name="sujet" id="subject" class="form-control" placeholder="Your subject of this message" required>
+                                </div>
+                            </div>
+
+                            <div class="row form-group">
+                                <div class="col-md-12">
+                                    <label for="message">Message</label>
+                                    <textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us" required></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value="Send Message" class="btn btn-primary"
+                            </div>
+
+                        </form>     
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                            <div class="col-md-12 text-center">
+                                <ul class="pagination">
+                                    <li class="disabled"><a href="#">&laquo;</a></li>
+                                    <li class="active"><a href="index.php">1</a></li>
+                                    <li><a href="blog.php">2</a></li>
+                                    <li><a href="blogpost.php">3</a></li>
+                                    <li><a href="ajouterblog.php">4</a></li>
+                                    <li><a href="modifierblog.php">5</a></li>
+                                    <li><a href="supprimerblog.php">6</a></li>
+                                    <li><a href="connexion.php">7</a></li>
+                                    <li><a href="#">&raquo;</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+            <div id="colorlib-instagram">
+            <div class="row">
+                <div class="col-md-12 col-md-offset-0 colorlib-heading text-center">
+                    <h2>Instagram</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="instagram-entry">
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-1.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-2.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-3.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-4.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-5.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-6.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-7.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-8.jpg);">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <footer id="colorlib-footer" role="contentinfo">
+            <div class="container">
+                <div class="row row-pb-md">
+                    <div class="col-md-4">
+                        <h2>Navigational</h2>
+                        <p>
+                            <ul class="colorlib-footer-links">
+                                <li><a href="index.php"><i class="icon-check"></i> Acceuil</a></li>
+                                <li><a href="blog.php"><i class="icon-check"></i> Blog</a></li>
+                                <li><a href="blogpost.php"><i class="icon-check"></i> Blog post</a></li>
+                                <li><a href="ajouterblog.php"><i class="icon-check"></i> Ajouter un blog</a></li>
+                                <li><a href="modifierblog.php"><i class="icon-check"></i> Modifier un blog</a></li>
+                                <li><a href="supprimerblog.php"><i class="icon-check"></i> Supprimer un blog</a></li>
+                                <li><a href="connexion.php"><i class="icon-check"></i> Connexion et enregistrement</a></li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>Recent Post</h2>
+                        <ul class="colorlib-footer-links">
+                            <li>
+                                <span>22 Jan, 2017</span>
+                                <a href="#">The Most Popular Leg Workout for Women</a>
+                            </li>
+                            <li>
+                                <span>20 Jan, 2017</span>
+                                <a href="#">Popular Lifestyle with Fashion &amp; Modeling</a>
+                            </li>
+                            <li>
+                                <span>21 Jan, 2017</span>
+                                <a href="#">Video Post Travel with my Friends</a>
+                            </li>
+                            <li>
+                                <span>20 Jan, 2017</span>
+                                <a href="#">Popular Lifestyle with Fashion &amp; Modeling</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-md-push-1">
+                        <h2>Tags</h2>
+                        <p class="tags">
+                            <span><a href="#"><i class="icon-tag"></i> Modeling</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Fashion</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Life</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Blog</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Workout</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Vacation</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Travel</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Exercise</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Health</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> News</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Model</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Women</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Animals</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Nature</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Art</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Sea</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Ocean</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Office</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Home</a></span>
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <p>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <small class="block">Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved. This template is made with <i class="icon-heart3" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></small>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+    </div>
+    
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="js/jquery.flexslider-min.js"></script>
+    <!-- Owl carousel -->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+    <!-- Main -->
+    <script src="js/main.js"></script>
+
+    </body>
+</html>
+
