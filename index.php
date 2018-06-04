82ba5f7902cdc72f4305b710f0e0e0c90ca2c45b
Davidjeje <david.marivat-larose@orange.fr>
2018-06-04 16:00:49 +0200

    Ajout du code de la page distributeur de la structure MVC

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
+<?php if ((!empty($_GET['page']) AND is_file('controleur/'.$_GET['page'].'.php') or $_GET['id']) or isset($_GET['id']))
+{
+	
+	if ($_GET['page'] == 'news') 
+    {
+    	include('controleur/'.$_GET['page'].'.php');
+        afficher();
+    } 
+
+    elseif ($_GET['page'] == 'admin' ) 
+    {
+    		include ('controleur/'.$_GET['page'].'.php');
+    		unblog();
+    	
+    }
+
+    elseif ( $_GET['id'] > 0)
+    {
+    	include ('controleur/'.$_GET['admin'].'.php');
+    	unblog();
+    	afficher();
+    }
+
+    elseif ($_GET['page'] == 'index') 
+    {
+
+        include('controleur/'.$_GET['page'].'.php');
+        administrateur();
+    }
+}
+   
+else
+{
+	include('controleur/index.php');
+	accueil();
+}
+?>
 
